1778e8411b6addc3a97d98ee42af4defadb8bc1f
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-03-27 12:15:50 -0500

    Created two ships

--- js/app.js
+++ js/app.js
@@ -1,1 +1,71 @@
 console.log("up and running")
+
+////Space Battle\\\\
+
+
+const USS_Assembly = {
+	hull: 20,
+	firepower: 5,
+	accuracy: .7,
+
+}
+USS_Assembly.hull()
+
+const alien_Ship = {
+	hull: Math.floor(Math.random() * 3) + 4,
+	firepower: Math.floor(Math.random() * 3) + 2,
+	accuracy: Math.floor(Math.random() * .6) + .8, //come back to this later
+	attack: function (){
+		return USS_Assembly.hull - alien_Ship.firepower;
+
+	}
+
+	
+}
+console.log(alien_Ship.accuracy);
+alien_Ship.attack()
+
+
+const game = () => {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
